a2f6e71ef6a6a61f6dbd926a65858de7c21cca86
Yordis Prieto <yordis.prieto@gmail.com>
2013-05-21 00:48:35 -0400

    Actualizando la documentacion del archivo Group.php

--- Band/Group.php
+++ Band/Group.php
@@ -2,189 +2,209 @@
 
 namespace ReportExpress\Band;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Group
- *
- * @author osley.rivera
+ * Group Class
+ * 
+ * This class contains the logic of the Group bands
+ * 
+ * @category    Library
+ * @package     ReportExpress
+ * @subpackage  Band
+ * @version     1.0 In development. Very unstable.
+ * @author      Yordis Prieto <yordis.prieto@gmail.com>
+ * @copyright   Creative Commons (CC) 2013, Yordis Prieto.
+ * @license     http://creativecommons.org/licenses/by-nc-sa/3.0/ Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
  */
 class Group {
 
-    /**
-     * Nombre del grupo.
-     * @var string 
-     */
-    private $name;
+   /**
+    * Name of group.
+    * @var string 
+    */
+   private $name;
 
-    /**
-     * La expresion que indica comienzo y fin del grupo.
-     * @var string 
-     */
-    private $groupExpression;
+   /**
+    * The expression that indicates the beginning and end of the group.
+    * @var string 
+    */
+   private $groupExpression;
 
-    /**
-     * Si comienza en una nueva pagina.
-     * @TODO No programada.
-     * @var boolean 
-     */
-    private $isStartNewPage;
+   /**
+    * If you start on a new page.
+    * @var boolean 
+    * 
+    * @TODO No programmed.
+    */
+   private $isStartNewPage;
 
-    /**
-     * Si resetea el numero de paginas.
-     * @TODO No programada.
-     * @var boolean 
-     */
-    private $isResetPageNumber;
+   /**
+    * If resetting the number of pages.
+    * @var boolean 
+    * 
+    * @TODO No programmed.
+    */
+   private $isResetPageNumber;
 
-    /**
-     * @TODO No programada.
-     * @var boolean 
-     */
-    private $isReprintHeaderOnEachPage;
+   /**
+    * @var boolean  
+    * 
+    * @TODO No programmed.
+    */
+   private $isReprintHeaderOnEachPage;
 
-    /**
-     * Se comporta como una banda y contiene los componentes a mostrar
-     * dentro del grupo.
-     * @var Band 
-     */
-    private $groupHeader = NULL;
+   /**
+    * It behaves like a band and contains the components 
+    * to show within the group.
+    * @var Band 
+    */
+   private $groupHeader = NULL;
 
-    /**
-     * Se comporta como una banda y contiene los componentes a mostrar
-     * dentro del grupo.
-     * @var Band 
-     */
-    private $groupFooter = NULL;
+   /**
+    * It behaves like a band and contains the components 
+    * to show within the group.
+    * @var Band 
+    */
+   private $groupFooter = NULL;
 
-    /**
-     * Contiene los valor despues de analizado el grupo.
-     * @var array 
-     */
-    private $value = array('header' => NULL, 'footer' => NULL);
+   /**
+    * Contains the value after analyzing the group.
+    * @var array 
+    */
+   private $value = array('header' => NULL, 'footer' => NULL);
 
-    public function __construct($data) {
+   /**
+    * Construtor of the class
+    * 
+    * @param \SimpleXmlElment $data.
+    * @return void
+    */
+   public function __construct($data) {
 
-        //attribute - simple
-        $this->name = (string) $data['name'];
-        $this->isStartNewPage = (boolean) $data['isStartNewPage'];
-        $this->isResetPageNumber = (boolean) $data['isResetPageNumber'];
-        $this->isReprintHeaderOnEachPage = (boolean) $data['isReprintHeaderOnEachPage'];
-        $this->groupExpression = isset($data->groupExpression) ? (string) $data->groupExpression : NULL;
+      //attribute - simple
+      $this->name = (string) $data['name'];
+      $this->isStartNewPage = (boolean) $data['isStartNewPage'];
+      $this->isResetPageNumber = (boolean) $data['isResetPageNumber'];
+      $this->isReprintHeaderOnEachPage = (boolean) $data['isReprintHeaderOnEachPage'];
+      $this->groupExpression = isset($data->groupExpression) ? (string) $data->groupExpression : NULL;
 
-        //attribute - object
-        if ($data->groupHeader && count($data->groupHeader->band->children()) > 0) {
-            $this->groupHeader = new Band($data->groupHeader->band);
-        }
-        if ($data->groupFooter && count($data->groupFooter->band->children()) > 0) {
-            $this->groupFooter = new Band($data->groupFooter->band);
-        }
-    }
+      //attribute - object
+      if ($data->groupHeader && count($data->groupHeader->band->children()) > 0) {
+	 $this->groupHeader = new Band($data->groupHeader->band);
+      }
+      if ($data->groupFooter && count($data->groupFooter->band->children()) > 0) {
+	 $this->groupFooter = new Band($data->groupFooter->band);
+      }
+   }
 
-    /**
-     * Devuelve el nombre del grupo
-     * @return string nombre.
-     */
-    public function name() {
-        return $this->name;
-    }
+   /**
+    * Return the name of group.
+    * 
+    * @return string nombre.
+    */
+   public function name() {
+      return $this->name;
+   }
 
-    /**
-     * Devuelve la expresion del grupo.
-     * @return string la expresion
-     */
-    public function groupExpression() {
-        return $this->groupExpression;
-    }
+   /**
+    * Return the expression of the group.
+    * 
+    * @return string The expresion.
+    */
+   public function groupExpression() {
+      return $this->groupExpression;
+   }
 
-    /**
-     * IsStartNewPage
-     * @return boolean TRUE o FALSE
-     */
-    public function isStartNewPage() {
-        return $this->isStartNewPage;
-    }
+   /**
+    * IsStartNewPage.
+    * 
+    * @return boolean The value.
+    */
+   public function isStartNewPage() {
+      return $this->isStartNewPage;
+   }
 
-    /**
-     * IsResetPageNumber
-     * @return boolean TRUE o FALSE
-     */
-    public function isResetPageNumber() {
-        return $this->isResetPageNumber;
-    }
+   /**
+    * IsResetPageNumber.
+    * 
+    * @return boolean The value.
+    */
+   public function isResetPageNumber() {
+      return $this->isResetPageNumber;
+   }
 
-    /**
-     * IsReprintHeaderOnEachPage
-     * @return boolean TRUE o FALSE
-     */
-    public function isReprintHeaderOnEachPage() {
-        return $this->isReprintHeaderOnEachPage;
-    }
+   /**
+    * IsReprintHeaderOnEachPage.
+    * 
+    * @return boolean The value.
+    */
+   public function isReprintHeaderOnEachPage() {
+      return $this->isReprintHeaderOnEachPage;
+   }
 
-    /**
-     * Devuelve el group header del grupo.
-     * @return Band
-     */
-    public function header() {
-        return $this->groupHeader;
-    }
+   /**
+    * Return the groupHeader of the group.
+    * 
+    * @return \ReportExpress\Band\Band The groupHeader band.
+    */
+   public function header() {
+      return $this->groupHeader;
+   }
 
-    /**
-     * Devuelve el group footer del grupo.
-     * @return Band
-     */
-    public function footer() {
-        return $this->groupFooter;
-    }
+   /**
+    * Return the groupFooter of the group.
+    * 
+    * @return \ReportExpress\Band\Band The groupFooter band.
+    */
+   public function footer() {
+      return $this->groupFooter;
+   }
 
-    /**
-     * Devuelve el valor que tiene el grupo.
-     * 
-     * @param string $name El nombre del grupo
-     * @return mixed El valor.
-     */
-    public function value($name) {
-        return $this->value[$name];
-    }
+   /**
+    * Return the value of the group.
+    * 
+    * @param string $name Name of the group.
+    * @return mixed The value.
+    */
+   public function value($name) {
+      return $this->value[$name];
+   }
 
-    /**
-     * Cambia el valor del grupo pasado en name.
-     * 
-     * @param string $name El nombre del grupo.
-     * @param mixed $value El nuevo valor.
-     */
-    public function setValue($name, $value) {
-        $this->value[$name] = $value;
-    }
+   /**
+    * Change the value of the group.
+    * 
+    * @param string $name The name of group.
+    * @param mixed $value The new value.
+    * @return void
+    */
+   public function setValue($name, $value) {
+      $this->value[$name] = $value;
+   }
 
-    /**
-     * Renderea el grupo pasado en name.
-     * 
-     * @param \ReportExpress\Core\ReportExpress $report EL reporte donde se renderea.
-     * @param \ReportExpress\Core\Point $point El punto donde comienza a renderearse.
-     * @param string $name Nombre del grupo.
-     * @return boolean TRUE si se rendereo completamente sino FALSE. 
-     */
-    public function render($report, $point, $name) {
+   /**
+    * Render the group.
+    * 
+    * @param \ReportExpress\ReportExpress $report The report which is rendered.
+    * @param \ReportExpress\Core\Point $point The point where it begins to render.
+    * @param string $name Name of group.
+    * @return boolean TRUE if it was render completely, FALSE otherwise. 
+    */
+   public function render($report, $point, $name) {
 
-        $group = $this->$name();
+      $group = $this->$name();
 
-        if ($group) {
+      if ($group) {
 
-            $h = $point->y + $group->height();
+	 $h = $point->y + $group->height();
 
-            if ($h < $point->mxb) {
-                $group->render($report, $point);
-                $point->y = $h;
-                return TRUE;
-            }
-            return FALSE;
-        }
-        return TRUE;
-    }
+	 if ($h < $point->mxb) {
+	    $group->render($report, $point);
+	    $point->y = $h;
+	    return TRUE;
+	 }
+	 return FALSE;
+      }
+      return TRUE;
+   }
 
 }
 
