d5d5560872654bce73caf56b03a4594f5dd55c48
Yordis Prieto <yordis.prieto@gmail.com>
2013-05-22 11:25:16 -0400

    Pasando trabajo por no saber git,

--- Component/Charts/AreaChart.php
+++ Component/Charts/AreaChart.php
@@ -10,7 +10,7 @@ namespace ReportExpress\Component\Charts;
  * @category    Library
  * @package     ReportExpress
  * @subpackage  Charts
- * @version     1.0 In development. Very unstable.
+ * @version     1.0
  * @author      Yordis Prieto <yordis.prieto@gmail.com>
  * @copyright   Creative Commons (CC) 2013, Yordis Prieto.
  * @license     http://creativecommons.org/licenses/by-nc-sa/3.0/ Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
